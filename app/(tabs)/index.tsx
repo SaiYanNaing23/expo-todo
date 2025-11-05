1cc48e2c74d986b694c0f9c03f1bb912552c3cf4
Sai Yan Naing <pesilay2372002@gmail.com>
2025-11-05 16:33:43 +0630

    dark-mode-and-add-logo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,34 @@
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import Header from "@/components/Header";
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
-import { Text, TouchableOpacity, View } from "react-native";
+import { useMutation, useQuery } from "convex/react";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <TouchableOpacity onPress={toggleDarkMode} >
-        <Text>
-          Dark Mode
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
+	const { toggleDarkMode, colors } = useTheme();
+	const homeStyles = createHomeStyles(colors);
+	const getTodos = useQuery(api.todos.getTodos);
+	console.log("todos", getTodos);
+	const addTodo = useMutation(api.todos.addTodo);
+	return (
+		<LinearGradient
+			colors={colors.gradients.background}
+			style={homeStyles.container}
+		>
+			<StatusBar barStyle={colors.statusBarStyle} />
+			<SafeAreaView style={homeStyles.safeArea}>
+				<Header />
+				<Text>Edit app/index.tsx to edit this screen.</Text>
+				<TouchableOpacity onPress={toggleDarkMode}>
+					<Text>Dark Mode</Text>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => addTodo({ text: "hello world." })}>
+					<Text>Add Todo</Text>
+				</TouchableOpacity>
+			</SafeAreaView>
+		</LinearGradient>
+	);
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,27 +3,26 @@ import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
-  unsavedChangesWarning: false,
+	unsavedChangesWarning: false,
 });
 
 export default function RootLayout() {
-  return (
-    <ConvexProvider client={convex}>
-    <ThemeProvider>
-    <Stack
-      screenOptions={{
-        headerShown: false
-      }}
-    >
-      <Stack.Screen
-        name="(tabs)"
-        options={{
-          title: "Home",
-        }}
-      />
-    </Stack>
-    </ThemeProvider>
-
-    </ConvexProvider>
-  );
+	return (
+		<ConvexProvider client={convex}>
+			<ThemeProvider>
+				<Stack
+					screenOptions={{
+						headerShown: false,
+					}}
+				>
+					<Stack.Screen
+						name="(tabs)"
+						options={{
+							title: "Home",
+						}}
+					/>
+				</Stack>
+			</ThemeProvider>
+		</ConvexProvider>
+	);
 }
