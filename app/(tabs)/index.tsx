f03c58dfed46086079fcde01acf20284ec30aef7
Sai Yan Naing <pesilay2372002@gmail.com>
2025-11-06 09:26:47 +0630

    add-todo-input

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { createHomeStyles } from "@/assets/styles/home.styles";
 import Header from "@/components/Header";
+import TodoInput from "@/components/TodoInput";
 import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
 import { useMutation, useQuery } from "convex/react";
@@ -21,6 +22,7 @@ export default function Index() {
 			<StatusBar barStyle={colors.statusBarStyle} />
 			<SafeAreaView style={homeStyles.safeArea}>
 				<Header />
+				<TodoInput />
 				<Text>Edit app/index.tsx to edit this screen.</Text>
 				<TouchableOpacity onPress={toggleDarkMode}>
 					<Text>Dark Mode</Text>
